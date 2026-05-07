111f746f84414421d9983759a818877bb9e2e3ee
hstefanov1 <269922586+hstefanov1@users.noreply.github.com>
2026-05-07 22:29:23 +0100

    feat(sec): add suite size validation over 10kb to keep app lightweight and fast

--- src/index.js
+++ src/index.js
@@ -11,6 +11,11 @@ if (!(await suiteFile.exists())) {
   error(`suite file not found \"${suitePath}\"`);
 }
 
+if (suiteFile.size > 10240) {
+  const kbSize = Math.round(suiteFile.size / 1024);
+  error(`suite file is too large (${kbSize}KB), please reduce it to under 10KB`);
+}
+
 const suite = await suiteFile.json();
 log(`version..: ${suite.version}`);
 log(`prompt...: ${suite.prompt}`);
